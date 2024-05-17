713569d4f0e4539db29c16ce528fe22d6174f489
SADAT BASINA SANI <sbsani@st.ug.edu.gh>
2024-05-17 19:50:47 +0000

    part of task 3

--- userInfo.js
+++ userInfo.js
@@ -1,1 +1,13 @@
 // userInfo.js file
+let id;
+id = 1;
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map(( name, index) => {
+        return {
+            originalName: name,
+            modifiedNames: modifiedNames[index],
+            id: id++,
+        };
+    });
+}
