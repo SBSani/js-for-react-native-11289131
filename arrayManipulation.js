3c3ca0d35d15939c719d4a9ccf270404593f5e92
SADAT BASINA SANI <sbsani@st.ug.edu.gh>
2024-05-17 19:39:18 +0000

    creation of task 3

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -6,3 +6,21 @@ function processArray(arr) {
 const result = processArray([5, 10, 15, 20]);
 console.log(result);
 // This gives us a result of [15, 100, 45, 400].
+
+// Task 2
+function formatArrayStrings(stringArr, numArr) {
+    return stringArr.map((str, index) => {
+        if (numArr[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+        });
+    }
+    
+// for example
+const strings = ['Kalu', 'Champ', 'Boss', 'Post'];
+const numbers = processArray([1, 2, 3, 4]);
+const formattedStrings = formatArrayStrings(strings, numbers); 
+console.log(formattedStrings);
+// the formatted strings gives us [ 'kalu', 'CHAMP', 'boss', 'POST' ]
