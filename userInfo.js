ddf55499592ffbec11c88f45ac3bf748338e474c
SADAT BASINA SANI <sbsani@st.ug.edu.gh>
2024-05-17 19:52:28 +0000

    task  3

--- userInfo.js
+++ userInfo.js
@@ -11,3 +11,10 @@ function createUserProfiles(names, modifiedNames) {
         };
     });
 }
+// for example, using the names and modified names in Task 2
+const number = [1, 2, 3, 4];
+const processedNumber = processArray(number);
+const names = ['King', 'fred', 'sheep', 'cat']
+const modifiedNames = formatArrayStrings(names, processedNumber)
+const userProfiles = createUserProfiles(names, modifiedNames);
+console.log(userProfiles);
